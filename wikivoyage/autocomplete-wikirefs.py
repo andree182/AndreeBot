a2a33da7e3510561a7dc4039cfb94611f4c94ca9
Andrej Krutak <dev@andree.sk>
2018-03-03 16:05:23 +0100

    Allow stopping via talk page, append wikidata before wikipedia, add blacklist

--- wikivoyage/autocomplete-wikirefs.py
+++ wikivoyage/autocomplete-wikirefs.py
@@ -9,8 +9,13 @@ from pywikibot import pagegenerators as pg
 import sys
 import mwparserfromhell
 import subprocess
+import binascii
+import os
 
-valid_templates = ["listing", "see", "do", "go", "eat", "buy", "sleep", "drink"]
+checkCommits = True
+botName = "AndreeBot"
+
+validTemplates = ["listing", "see", "do", "go", "eat", "buy", "sleep", "drink"]
 
 site = pywikibot.Site("en", "wikivoyage")
 #repo = site.data_repository()
@@ -18,39 +23,94 @@ site = pywikibot.Site("en", "wikivoyage")
 wsite = pywikibot.Site("en", "wikipedia")
 
 def processPage(title):
-	print("Processing %s..." %(title))
+	failures = []
+	print("\nProcessing %s..." %(title))
 
 	page = pywikibot.Page(site, title)
 	parsed = mwparserfromhell.parse(page.text)
 
 	for template in parsed.filter_templates():
-		if template.name.strip() in valid_templates:
+		if template.name.strip() in validTemplates:
 			paramNames = [param.name.lower().strip() for param in template.params]
 			if 'wikipedia' in paramNames and not 'wikidata' in paramNames:
 				# fill it in
 				wIdx = paramNames.index('wikipedia')
-				wikipedia = template.params[wIdx].value
+				wikipedia = template.params[wIdx].value.strip()
+				if wikipedia == "":
+					failures += [wikipedia]
+					continue
+				if '#' in wikipedia:
+					failures += [wikipedia]
+					# Wikidata to part of wikipedia article doesn't exist...
+					continue
+
 				wpage = pywikibot.Page(wsite, wikipedia)
+				if wpage.isRedirectPage():
+					wpage = wpage.getRedirectTarget()
+				if wpage.pageid == 0:
+					print("!!! non-existent wikipedia article '%s'" % (wikipedia))
+					failures += [wikipedia]
+					continue
+
 				witem = pywikibot.ItemPage.fromPage(wpage)
 				wID = witem.getID()
-				template.add("wikidata", wID)
+				template.add("wikidata", wID, before="wikipedia")
 
-	newText = str(parsed)
-	open('f.1', 'w').write(page.text)
-	open('f.2', 'w').write(newText)
-	subprocess.call("diff -pdau f.1 f.2 | grep '^[+-]'", shell=True)
+	commit = True
+	if checkCommits:
+		# show diff, filter out most-likely-correct changes
+		newText = str(parsed)
+		open('f.1', 'w').write(page.text)
+		open('f.2', 'w').write(newText)
+		subprocess.call("diff -pdau f.1 f.2 | grep -v '^[-+][-+][-+] f\.[12]'| grep '^[+-]' > f.diff", shell=True)
+		diff = open('f.diff').read().split('\n')
+		diff = list(filter(lambda l: l.strip() != '-}}', diff))
+		diff = list(filter(lambda l: not l.startswith('+| wikidata=') and not l.startswith('+| wikidata ='), diff))
+		diff = list(filter(lambda l: l!='', diff))
 
-	input("Do Ctrl+C or enter to upload...")
+		if diff != []:
+			subprocess.call("kdiff3 f.1 f.2", shell=True)
+			commit = (input("Enter 'ok' to upload...") == 'ok')
 
-	page.text = newText
-	page.save("Add wikidata ID(s) derived from wikipedia parameter(s)")
+	if commit:
+		page.text = newText
+		page.save("Add wikidata ID(s) derived from wikipedia parameter(s)", botflag = True)
+	return failures
 
-def processList(page):
-	cat = pywikibot.Category(site, 'Listing_with_Wikipedia_link_but_not_Wikidata_link')
+def processList(cat):
+	timestamp = pywikibot.Page(site, "User_talk:" + botName).getVersionHistory()[0].timestamp
+	counter = 0
+	counterNum = 1
+
+	if not os.path.exists("failed"):
+		open("failed", "w")
+
+	exceptions = {}
+	
+	for l in open("failed", "r").read().split('\n'):
+		i = l.split('|')
+		if i == ['']:
+			continue
+		exceptions[i[0]] = i[1]
+	exceptionsf = open("failed", "a")
+	
+	cat = pywikibot.Category(site, cat)
 	for a in cat.articles():
 		if ':' in a.title():
 			# skip the obvious internal stuff (templates, talk, user pages...)
 			continue
-		processPage(a.title())
+		if a.title() in exceptions:
+			continue
+		fails = processPage(a.title())
+		if fails != []:
+			exceptionsf.write("%s|%s\n" % (a.title(), ', '.join(fails)))
+			exceptionsf.flush()
+		
+		counter += 1
+		if counter == counterNum:
+			counter = 0
+			if pywikibot.Page(site, "User_talk:" + botName).getVersionHistory()[0].timestamp > timestamp:
+				print("Got kicked by someone!")
+				break
 
-processList("Category:Listing_with_Wikipedia_link_but_not_Wikidata_link")
+processList("Listing_with_Wikipedia_link_but_not_Wikidata_link")
